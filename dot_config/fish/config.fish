503ecf1ce6e0d2023293ab15b771d6fae16315a1
twells46 <173561638+twells46@users.noreply.github.com>
2026-02-23 06:19:29 -0600

    Updates for gcloud and kitty

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -7,3 +7,6 @@ if status is-interactive
     # Set fish_key_bindings globally per 4.3 update instructions
     set --global fish_key_bindings fish_default_key_bindings
 end
+
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f '/var/home/tom/.local/opt/google-cloud-sdk/path.fish.inc' ]; . '/var/home/tom/.local/opt/google-cloud-sdk/path.fish.inc'; end
