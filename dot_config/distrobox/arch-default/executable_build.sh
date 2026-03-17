005ee9cc95dd1379840665b72cb76a69d216c882
twells46 <173561638+twells46@users.noreply.github.com>
2026-03-17 16:30:21 -0500

    Distrobox configurations

---
+++ dot_config/distrobox/arch-default/executable_build.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+set -eu
+
+if [ "$#" -ne 1 ]; then
+	printf "1 arg required\n"
+	printf "usage: ./build.sh container-tag:ver\n"
+	exit 1
+fi
+
+podman build -t "${1}" .
+
+printf "Updating distrobox.ini...\n"
+sed -i "s|^image=.*\$|image=${1}|" ./distrobox.ini
+printf "Updated!\n"
