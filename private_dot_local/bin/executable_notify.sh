6378ea2488b2287913cae43aafb1de84b7971d3a
twells46 <173561638+twells46@users.noreply.github.com>
2026-04-02 13:40:12 -0500

    asdf

---
+++ private_dot_local/bin/executable_notify.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env dash
+
+if [ -n $WAYLAND_DISPLAY ]; then
+	herbe="wayherb"
+elif [ -n $DISPLAY ]; then
+	herbe="herbe"
+else
+	printf "Error: No display server found\n" >&2
+	exit 1
+fi
+
+tiramisu -o '#source	#summary	#body' |
+while read -r line; do
+	head="${line%	*}"
+	src="${head%	*}"
+	sum="${head#*	}"
+	bod="${line##*	}"
+	$herbe "${src} | ${sum}: ${bod}"
+	# "$(printf '%s' "${line}" | awk -F '\t' '{printf "%s | %s: %s", $1, $2, $3}')"
+done
