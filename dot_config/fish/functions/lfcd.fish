32db3ce408a81868d66167670c20d6ee19295c40
twells46 <tom@wellsth.com>
2026-05-24 07:51:07 -0500

    2026-05-24

--- dot_config/fish/functions/lfcd.fish
+++ dot_config/fish/functions/lfcd.fish
@@ -14,5 +14,9 @@
 function lfcd --wraps="lf" --description="lf - Terminal file manager (changing directory on exit)"
     # `command` is needed in case `lfcd` is aliased to `lf`.
     # Quotes will cause `cd` to not change directory if `lf` prints nothing to stdout due to an error.
-    cd "$(command lf -print-last-dir $argv)"
+    if command -q ueberzug
+        cd "$(command lf-ubz -print-last-dir $argv)"
+    else
+        cd "$(command lf -print-last-dir $argv)"
+    end
 end
